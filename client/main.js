454dbf0eaed63498f0d62ceb705115674c58f0a8
IRGames <isaac.w.raymond@gmail.com>
2018-03-18 08:29:44 -0400

    Facebook sucks

--- client/main.js
+++ client/main.js
@@ -9,7 +9,7 @@ Meteor.startup(() => {
 
 window.fbAsyncInit = function() {
   FB.init({
-    appId      : '2410241845668819',
+    appId      : '1581525701943620',
     cookie     : true,
     xfbml      : true,
     version    : 'v2.10'
@@ -27,34 +27,18 @@ window.fbAsyncInit = function() {
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
 
-function statusChangeCallback(response) {
-  console.log('statusChangeCallback');
-  console.log(response);
-  // The response object is returned with a status field that lets the
-  // app know the current login status of the person.
-  // Full docs on the response object can be found in the documentation
-  // for FB.getLoginStatus().
-  if (response.status === 'connected') {
-    // Logged into your app and Facebook.
-    testAPI();
-  } else {
-    // The person is not logged into your app or we are unable to tell.
-    document.getElementById('status').innerHTML = 'Please log ' +
-      'into this app.';
-  }
-}
 
-function testAPI() {
-  console.log('Welcome!  Fetching your information.... ');
-  FB.api('/me', function(response) {
-    console.log('Successful login for: ' + response.name);
-    document.getElementById('status').innerHTML =
-      'Thanks for logging in, ' + response.name + '!';
-  });
-}
-
-function checkLoginState() {
-  FB.getLoginStatus(function(response) {
-    statusChangeCallback(response);
-  });
-}
+Template.login.events({
+    'click button.log-in'(event) {
+        event.preventDefault();
+        Meteor.loginWithFacebook({requestPermissions: ['public_profile', 'email']}, function(err){
+            if (err) {
+                console.log('Handle errors here: ', err);
+            }
+        });
+    },
+    'click button.log-out'(event) {
+        event.preventDefault();
+        Meteor.logout();
+    }
+});
